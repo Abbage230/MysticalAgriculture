783046fcefa1d7077ce50a620624bde09414a44d
BlakeBr0 <luasmrks@gmail.com>
2024-11-19 15:58:10 -0600

    Fix Absorption Augment applying instantly when hearts are depleted closes #747

--- src/main/java/com/blakebr0/mysticalagriculture/augment/AbsorptionAugment.java
+++ src/main/java/com/blakebr0/mysticalagriculture/augment/AbsorptionAugment.java
@@ -13,6 +13,7 @@ import net.minecraft.world.level.Level;
 import java.util.EnumSet;
 
 public class AbsorptionAugment extends Augment {
+    private static final String PERSISTENT_DATA_KEY = "MA_AbsorptionAugment";
     private final int amplifier;
 
     public AbsorptionAugment(ResourceLocation id, int tier, int amplifier) {
@@ -22,12 +23,24 @@ public class AbsorptionAugment extends Augment {
 
     @Override
     public void onArmorTick(ItemStack stack, Level level, Player player) {
-        if (!player.hasEffect(MobEffects.ABSORPTION)) {
+        if (level.isClientSide() || player.hasEffect(MobEffects.ABSORPTION))
+            return;
+
+        if (getLastApplicationTime(player) + 9600L < level.getGameTime()) {
             player.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 9600, this.amplifier, true, false));
+            setLastApplicationTime(player, level.getGameTime());
         }
     }
 
     private static int getColor(int color, int tier) {
         return ColorHelper.saturate(color, Math.min((float) tier / 5, 1));
+    }
+
+    private static long getLastApplicationTime(Player player) {
+        return player.getPersistentData().getLong(PERSISTENT_DATA_KEY);
+    }
+
+    private static void setLastApplicationTime(Player player, long time) {
+        player.getPersistentData().putLong(PERSISTENT_DATA_KEY, time);
     }
 }
